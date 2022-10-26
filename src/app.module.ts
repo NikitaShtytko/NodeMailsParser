195ed4b5665efd34d4128b6577fa626372ae9fc5
olegkrasilev <olegkrasilev@gmail.com>
2022-10-26 14:37:16 +0300

    feat(app): configModule options

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ImapModule } from './imap/imap.module';
 
 @Module({
-  imports: [ImapModule],
+  imports: [
+    ImapModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
